0ef47fd7ce885564d652f1c79223ee09cb1dfc45
Peter <user@users-MacBook-Air.local>
2023-06-08 08:53:04 +0100

    one more change

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const s = Deno.listen({ port: 8000 });
 console.log("Listening on http://localhost:8000/");
 
-(async () => {
-  for (const conn of s) {
+for await (const conn of s) {
   await conn.respond({ body: "Welcome to Deno\n" });
-  }
-})();
+}
