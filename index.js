e82bd59b28ef5990d665d3540e89c1a6cf969143
Peter <user@users-MacBook-Air.local>
2023-06-08 07:35:05 +0100

    more changes

---
+++ index.js
@@ -0,0 +1,7 @@
+<script src="https://deno.land/std/http/server.ts"></script>
+const s = Deno.listen({ port: 8000 });
+console.log("Listening on http://localhost:8000/");
+
+for await (const conn of s) {
+  await conn.respond({ body: "Welcome to Deno\n" });
+}
--- server.js
+++
@@ -1,7 +0,0 @@
-addEventListener("fetch", (event) => {
-  event.respondWith(handleRequest(event.request));
-});
-
-async function handleRequest(request) {
-  return new Response("Welcome to Deno\n", { status: 200 });
-}
