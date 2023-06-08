f2558103518b8c2a1ca5ab8a2028eef602531b3c
Peter <user@users-MacBook-Air.local>
2023-06-08 09:19:50 +0100

    one more time

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const s = Deno.listen({ port: 8000 });
+const server = Deno.listen({ port: 8000 });
 console.log("Listening on http://localhost:8000/");
 
-for await (const conn of s) {
-  await conn.respond({ body: "Welcome to Deno\n" });
+for await (const conn of server) {
+  const httpConn = Deno.serveHttp(conn);
+  for await (const { respondWith } of httpConn) {
+    await respondWith(new Response("Welcome to Deno\n"));
+  }
 }
